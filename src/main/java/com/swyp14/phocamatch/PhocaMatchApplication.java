1fea44412e05d10614c91c95e6825d47f553378f
donghan378 <ehdgksqorgh@naver.com>
2026-07-18 15:57:29 +0900

    Initial commit

---
+++ src/main/java/com/swyp14/phocamatch/PhocaMatchApplication.java
@@ -0,0 +1,13 @@
+package com.swyp14.phocamatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhocaMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhocaMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swyp14/phocamatch/PhocaMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp14.phocamatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhocaMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
